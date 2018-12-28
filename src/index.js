072e4827d0e99eefcf38f7839fb712859ee963fb
eltneg <yorewole@gmail.com>
2018-12-28 20:16:26 +0100

    Set up ping route and empty test

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// root
+app.route("/").get((req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: "eltneg's dev ops test project"
+  });
+});
+
+//ping route
+app.route("/ping").get((req, res) => {
+  res.status(200).json({
+    success: true,
+    msg: "pong"
+  });
+});
+
+// other routes
+app.route("*").all((req, res) =>
+  res.status(404).json({
+    success: false,
+    message: "route not found"
+  })
+);
+
+const server = app.listen(port, () => console.debug(`"app running on port ${port}"`));
+
+// console.log(`Serving app on: http://localhost:${port}`);
+
+export default server;
